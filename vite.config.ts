d945a68a7ff31438b152c9e12dfae9971476e881
Keplej <Johnny.Keple@gmail.com>
2025-01-22 06:27:58 -0600

    add: dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Keplej-github-io/",
+  base: "/Keplej.github.io/",
 })
